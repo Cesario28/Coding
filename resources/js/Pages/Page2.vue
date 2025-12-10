b89b4d63dca22dbd265f2e65a2cebd27875c7dc9
Cesario28 <2302791@ub.edu.ph>
2025-12-10 17:29:05 +0800

    Update Page2.vue

--- resources/js/Pages/Page2.vue
+++ resources/js/Pages/Page2.vue
@@ -1,10 +1,16 @@
-<script>
-import { defineComponent } from '@vue/composition-api'
+<script setup>
+import { Link } from "@inertiajs/vue3";
 
-export default defineComponent({
-    setup() {
-        
-        
-    },
-})
+defineProps({
+  total: Number,
+});
 </script>
+
+<template>
+  <h1>Page2</h1>
+
+  <p>Total price:{{ total }}</p>
+  <Link href="/page1" class="text-gray-500 text-sm mb-4 inline-block"
+    >&larr; Back to list</Link
+  >
+</template>
